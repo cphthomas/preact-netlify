7f387071bb3829dbf6958d452e41949456f814be
Thomas Petersen <10553816+cphthomas@users.noreply.github.com>
2020-11-18 11:22:30 +0100

    Update index.js

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -33,8 +33,7 @@ const Home = () => {
 			</div>
 			<div class={style.bio}>
 				<p class={style.bioleft}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					TESTTEST tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
 					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
 					aliquip ex ea commodo consequat. Duis aute irure dolor in
 					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
